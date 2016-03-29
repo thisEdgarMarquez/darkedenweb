262bcce5aa2d7043c39892ff2e1863fc0d3ed8ac
thisoneblaack <blaackhat@gmail.com>
2016-03-28 23:38:44 -0430

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$db_host = "";
+$db_user = "";
+$db_pass = "";
+$open_web = 1; // Web Abierta : 1 - Web Cerrada : 0
+$open_market = 1; // Market Abierto : 1 - Market Cerrado : 0
+$db_name = "";
+?>
